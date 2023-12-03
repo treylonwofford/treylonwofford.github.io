ed07d2a6c26f2f2815d44ff37cd6e11c9d535163
Treylon Wofford <treylonwofford2@gmail.com>
2023-12-02 23:59:26 -0500

    first

---
+++ images/index.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ['ML Engineer', 'AI Engineer', 'Prompt Engineer.'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop: true
+  };
+var typed = new Typed('.typing', options);
